7721891eb1c521004ff37428190452c5098f3057
timemation <42440757+timmingbycoding@users.noreply.github.com>
2018-08-17 07:53:58 +0800

    past7
    这里存放学习时编写的代码

---
+++ 7.29/commen.js
@@ -0,0 +1,45 @@
+function my$(id){
+	return document.getElementById(id);
+}
+function  addEventListener(element,type,funName){
+	if(element.addEventListener){
+		element.addEventListener(type,funName,false);
+	}else if(element.attachEvent){
+		element.attachEvent("on"+type,funName);
+	}else{
+		element["on"+type]=funName;
+	}
+}
+function  removeEventListener(element,type,funName){
+	if(element.removeEventListener){
+		element.removeEventListener(type,funName,false);
+	}else if(element.detachEvent){
+		element.detachEvent("on"+type,funName);
+	}else{
+		element["on"+type]=null;
+	}
+}
+function setInnerText(element,str){
+	if(typeof(element.textContent)=="undefined"){
+		element.innerText=str;
+	}else {
+        element.textContent = str;
+    }
+}
+
+
+
+function standardMove(element,target){
+	var step=2;
+	var locationX=element.offsetLeft;
+	step=locationX<target?step:-step;
+	element.timeMove=setInterval(function(){
+		locationX+=step;
+		if(Math.abs(locationX-target)>Math.abs(step)){
+			element.style.left=locationX+"px";	
+		}else{
+			clearInterval(element.timeMove);
+			element.style.left=target+"px";
+		}
+	},10)
+}
